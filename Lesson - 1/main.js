a8b1b41ef7b3dd0dad699ad73b2622136cec5268
Aleksey <zonerage007@gmail.com>
2018-10-11 14:26:36 +0300

    Все исправленно

--- Lesson - 1/main.js
+++ Lesson - 1/main.js
@@ -3,23 +3,23 @@
 let money = prompt( 'Ваш бюджет на месяц?', '' ),
     time = prompt( 'Введите дату в формате YYYY-MM-DD', '' );
 
+let appData = {
+    budjet: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+}
+
 let questionOne = prompt( 'Введите обязательную статью расходов в этом месяце?', ''),
     questionTwo = prompt( 'Во сколько обойдется?', '' );
     questionThree = prompt( 'Введите обязательную статью расходов в этом месяце?', ''),
     questionFour = prompt( 'Во сколько обойдется?', '' );
 
-let appData = {
-  budjet: money,
-  timeData: time,
-  expenses: {
-    questionOne: questionTwo,
-    questionThree: questionFour
-  },
-  optionalExpenses: {},
-  income: [],
-  savings: false
-}
-
+appData.expenses.questionOne = questionTwo;
+appData.expenses.questionThree = questionFour;
+console.log(appData.expenses.questionOne);
 let moneyOnDay = appData.budjet / 30;
 
 alert(moneyOnDay);
